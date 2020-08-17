a1de17a880b7b5879a51cdcec02621502d2583bb
youssef <yn-neinaa@hotmail.com>
2020-08-17 18:48:01 +0000

    use links of plugins from cdn

--- public/dist/js/veevalidate-instanse.js
+++
@@ -1,8 +0,0 @@
-import Vue from "Vue";
-Vue.use(VeeValidate, {
-  classes: true,
-  classNames: {
-    valid: "is-valid",
-    invalid: "is-invalid"
-  }
-}); // use the validation of veeValidate package
